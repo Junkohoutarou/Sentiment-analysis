36ea594835d9a32e59bf4dac38fec5d0624c50e9
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 08:13:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,27 @@
 import streamlit as st
-import pandas as pd
-import io
-import matplotlib.pyplot as plt
-import seaborn as sns
+from PIL import Image 
+import pickle as pkl
+import numpy as np
 
-st.title('Data visualization')
-st.header('Upload data file')
-data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
 
-if data_file is not None:
-    df = pd.read_csv(data_file)
+class_list = {'0': 'Normal', '1': 'Pneumonia'}
 
-    st.header('Show data')
-    st.dataframe(df)
+st.title('Pneumonia Detection')
 
-    st.header('Descriptive statistics')
-    st.table(df.describe())
+input = open('lrc_xray.pkl','rb')
+model = pkl.load(input)
 
-    st.header('Show data information')
-    buffer = io.StringIO()
-    df.info(buf=buffer)
-    st.text(buffer.getvalue())
+st.header('Upload an image')
+image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']))
 
-    st.header('Visualize each attribute')
-    for col in df.columns:
-        fig, ax = plt.subplots()
-        ax.hist(df[col], bins=20)  
-        plt.xlabel(col)
-        plt.ylabel('Quantity')
-        st.pyplot(fig)
+if image is not None:
+  image = Image.open(image)
+  st.image(image, caption='Test image')
 
-    st.header('Show correlation between variables')
-    
-    # Xóa các dòng chứa giá trị NaN trước khi tính toán ma trận tương quan
-    df_corr = df.dropna()
+  if st.button('Predict'):
+    image = image.resize((227*227*3,1))
+    vector = np.array(image)
+    label= str((model.predict(vector))[0])
 
-    # Kiểm tra xem DataFrame có đủ dữ liệu để tính toán không
-    if len(df_corr) >= 2 and len(df_corr.columns) >= 2 and df_corr.select_dtypes(include='number').shape[1] >= 2:
-        fig, ax = plt.subplots()
-        sns.heatmap(df_corr.corr(method='pearson'), ax=ax, vmax=1, square=True, annot=True, cmap='Reds')
-        st.write(fig)
-    else:
-        st.warning('Not enough numeric data to calculate correlation matrix.')
-
-    output = st.radio('Choose a dependent variable', df.columns)
-    st.header('Show correlation between variables')
-    for col in list(df.columns):
-        if col != output:
-            fig, ax = plt.subplots()
-            ax.scatter(x=df[col], y=df[output])
-            plt.xlabel(col)
-            plt.ylabel(output)
-            st.pyplot(fig)
+    st.header('Result')
+    st.text(class_list[label])
