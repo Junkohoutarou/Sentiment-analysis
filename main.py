040c985fa73c78b224d5f01c53a5af1d7eff3c5d
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 17:05:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
+from PIL import Image
 import pickle as pkl
 import numpy as np
-from PIL import Image
 
-class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
-st.title("Sentiment analysis from Vietnamese student's feedback")
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positve'}
 
-#image = Image.open('ten.png')
-#st.image(image)
+st.title('Sentiment analysis from Vietnamese students’ feedback')
+
+image = Image.open('vsfc.jpg')
+st.image(image)
 
 input_ec = open('ec_vsfc.pkl', 'rb')
 encoder = pkl.load(input_ec)
@@ -19,9 +20,9 @@ st.header('Write a feedback')
 txt = st.text_area('', '')
 
 if txt != '':
-  if st.button('Predict'):
-    feature_vector = encoder.transform([txt])
-    label = str((model.predict(feature_vector))[0])
+    if st.button('Predict'):
+        feature_vector = encoder.transform([txt])
+        label = str((model.predict(feature_vector))[0])
 
-    st.header('Result')
-    st.text(class_list[label])  # Added the missing parenthesis and bracket here
+        st.header('Result')
+        st.text(class_list[label])
