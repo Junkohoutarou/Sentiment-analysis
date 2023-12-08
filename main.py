5a00fd935264a4cce2c1d8b492403782f87b835b
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 08:30:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pickle as pkl
 import numpy as np
 
 
-class_list = {'0': 'Negative', '1': 'Neutral', '2':"Positive'}
+class_list = {'0': 'Negative', '1': 'Neutral', '2':'Positive'}
 
 st.title('Sentiment analysis from Vietnamwsw students' feedback')
 
