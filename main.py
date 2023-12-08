e3e9826942dee4bd48deff57c001fda9f5a6f086
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 08:49:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class_list = {'0': 'Negative', '1': 'Neutral', '2':'Positive'}
 
-st.title('Sentiment analysis from Vietnamwsw students feedback')
+st.title("Sentiment analysis from Vietnamwsw student's feedback")
 
 input_ec = open('ec_vsfc.pkl','rb')
 encoder = pkl.load(input_ec)
